d6b5c33cdfff9d79b9926d39a024f47e19752a62
창훈 박 <41935668+wawworld72@users.noreply.github.com>
2022-05-07 10:27:31 +0000

    beta 정리

--- Makefile
+++ Makefile
@@ -16,11 +16,20 @@ EXE := $(BIN_DIR)/a.out
 SRC := $(wildcard $(SRC_DIR)/*.cpp)
 OBJ := $(SRC:$(SRC_DIR)/%.cpp=$(OBJ_DIR)/%.o)
 
-CPPFLAGS := -Iinclude -MMD -MP -I$(INC_DIR) -I$(SDL_INC) 
-CFLAGS   := -Wall
+CPPFLAGS := -Iinclude -MMD -MP -I$(INC_DIR) -I$(SDL_INC)
+CFLAGS   := -Wall 
 LDFLAGS  := -Llib
 LDLIBS   := -lm
-SDL_LIBS := -L$(SDL_LIB)/libSDL2_2.0.so -lSDL2 -L$(SDL_LIB)/libSDL2_image-2.0.so $(SDL_LIB)/libSDL2_image.a -L $(SDL_LIB)/libSDL2_gfx.so $(SDL_LIB)/libSDL2_gfx.a -L $(SDL_LIB)/libSDL2_ttf-2.0.so $(SDL_LIB)/libSDL2_ttf.a -L $(SDL_LIB)/libSDL2_mixer-2.0.so  $(SDL_LIB)/libSDL2_mixer.a -lfreetype -lpng -ljpeg -ltiff -lwebp 
+
+#SDL_LIBS := -L$(SDL_LIB)/libSDL2_2.0.so -lSDL2 -L$(SDL_LIB)/libSDL2_image-2.0.so $(SDL_LIB)/libSDL2_image.a -L $(SDL_LIB)/libSDL2_gfx.so $(SDL_LIB)/libSDL2_gfx.a -L $(SDL_LIB)/libSDL2_ttf-2.0.so $(SDL_LIB)/libSDL2_ttf.a  -L $(SDL_LIB)/libSDL2_mixer-2.0.so $(SDL_LIB)/libSDL2_mixer.a -lfreetype -lpng -ljpeg -ltiff -lwebp
+
+#mixer install 후 실행 가능 버전 
+SDL_LIBS := -L$(SDL_LIB)/libSDL2_2.0.so -lSDL2 -L$(SDL_LIB)/libSDL2_image-2.0.so $(SDL_LIB)/libSDL2_image.a -L $(SDL_LIB)/libSDL2_gfx.so $(SDL_LIB)/libSDL2_gfx.a -L $(SDL_LIB)/libSDL2_ttf-2.0.so $(SDL_LIB)/libSDL2_ttf.a -lSDL2_mixer -lfreetype -lpng -ljpeg -ltiff -lwebp
+
+
+# -L $(SDL_LIB)/libFLAC.so   -L $(SDL_LIB)/libogg.so.2.6.0  $(SDL_LIB)/libogg.a -L $(SDL_LIB)/libfluidsynth.so -L $(SDL_LIB)/libSDL2_mixer-2.0.so  $(SDL_LIB)/libSDL2_mixer.a  $(SDL_LIB)/libFLAC.a
+
+MIXER = libmikmod-dev libfishsound1-dev libsmpeg-dev liboggz2-dev libflac-dev libfluidsynth-dev libsdl2-mixer-dev libsdl2-mixer-2.0-0 libflac-dev
 
 .PHONY: all clean
 
@@ -38,7 +47,16 @@ $(BIN_DIR) $(OBJ_DIR):
 clean:
 	@$(RM) -rv $(BIN_DIR) $(OBJ_DIR)
 
+install:
+	install-pkg $(MIXER)
+
+setup:
+	export LIBRARY_PATH=$(SDL_LIB)
+	export LD_LIBRARY_PATH=$(SDL_LIB)
 -include $(OBJ:.o=.d)
 
 run: $(EXE)
 	$(EXE)
+
+#export LIBRARY_PATH=/home/runner/.apt/usr/lib/x86_64-linux-gnu:/home/runner/.apt/usr/lib/i386-linux-gnu:/usr/local/lib:/home/runner/.apt/usr/lib:/home/runner/HoseoSDL/3rdParty/SDL/lib/x86_64-linux-gnu
+#export LD_LIBRARY_PATH=/home/runner/.apt/usr/lib/x86_64-linux-gnu:/home/runner/.apt/usr/lib/i386-linux-gnu:/usr/local/lib:/home/runner/.apt/usr/lib:/home/runner/HoseoSDL/3rdParty/SDL/lib/x86_64-linux-gnu
--- src/main.cpp
+++ src/main.cpp
@@ -1,116 +1,183 @@
-#include "main.h"
+//#include "main.h"
 #include <stdio.h>
-
+#include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
 
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 bool g_bRunning = false;
 
-SDL_Texture* m_pTexture; 
-// 원본 사각형
-SDL_Rect m_sourceRectangle;	 
-// 대상 사각형
-SDL_Rect m_destinationRectangle;
+SDL_Texture* texureImage;
+SDL_Rect rectImage;
 
-SDL_Texture* text_texture;
-SDL_Rect r;
-//#include <iostream>
-bool init(const char* title, int xpos, int ypos, 
-          int height, int width, int flags)
+SDL_Texture* texureText;
+SDL_Rect rectText;
+
+TTF_Font* m_pFont;
+Mix_Chunk* chunk;
+bool bLeftMousePressed = false;
+
+bool init();
+void handleInput();
+void update();
+void render();
+
+int main(int argc, char* argv[])
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
-    {
-
-        g_pWindow = SDL_CreateWindow( title, 
-                                      xpos, ypos, 
-                                      height, width, flags);
-
-        if (g_pWindow != 0)
-        {
-            g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
-        }
-
+    if (!init()) {
+			return 1; // something's wrong
     }
-    else
-    {
+
+		g_bRunning = true;
+
+		// SDL Image
+    SDL_Surface* pTempSurface = IMG_Load("./assets/animate-alpha.png");
+		if (pTempSurface == 0) {
+			return false;
+		}
+    texureImage = SDL_CreateTextureFromSurface(g_pRenderer, pTempSurface);
+    SDL_FreeSurface(pTempSurface);
+		rectImage = { 0, 0, 128, 82 };
+
+		// SDL TTF
+		SDL_Color color_Blue = { 0, 0, 255 };
+    SDL_Color color_White = { 255, 255, 255 };
+	  SDL_Surface* temp_surf = TTF_RenderUTF8_Shaded(m_pFont, "한글 텍스쳐", color_Blue, color_White);
+		if (temp_surf == 0) {	
+				SDL_Log("%s", TTF_GetError());
+        return false;
+    }
+	  texureText = SDL_CreateTextureFromSurface(g_pRenderer, temp_surf);
+    SDL_FreeSurface(temp_surf);
+    rectText = { 0, 0, temp_surf->w, temp_surf->h };
+
+		// SDL Mixer
+		// chunk에 WAV 로드, 재생은 update()에서
+    // 참고: 배경음악은 Mix_Music* music = Mix_LoadMUS("FILE");
+    chunk = Mix_LoadWAV("./assets/jump.wav");
+		if (chunk == 0) {	
+				SDL_Log("%s", Mix_GetError());
         return false;
     }
 
-    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-
-  SDL_Surface* pTempSurface = IMG_Load("./assets/animate-alpha.png");
-  m_pTexture = SDL_CreateTextureFromSurface(g_pRenderer, pTempSurface);
-  SDL_FreeSurface(pTempSurface);
-
-  m_sourceRectangle.w = 128;
-  m_sourceRectangle.h = 82;
-
-  m_destinationRectangle.w = m_sourceRectangle.w;
-  m_destinationRectangle.h = m_sourceRectangle.h;
-
-    m_destinationRectangle.x = m_sourceRectangle.x = 0;
-    m_destinationRectangle.y = m_sourceRectangle.y = 0;
-		
-TTF_Font* m_pFont;
+		// 볼륨 설정, -1은 모든 채널
+    Mix_Volume(-1, 128);
 	
-	  if (TTF_Init() < 0) { printf("오류: 폰트를 초기화할 수 없습니다. (%s)\n", TTF_GetError()); return false; }
-  m_pFont = TTF_OpenFont("./assets/NanumGothic.ttf", 20);
-  if (m_pFont == NULL) { printf("오류: 폰트를 열 수 없습니다. (%s)\n", TTF_GetError()); return false; }
-
-	  SDL_Color color_Black = { 0, 0, 0 };
-    SDL_Color color_Blue = { 0, 0, 255 };
-	  SDL_Color color_White = { 255, 255, 255 };
-    SDL_Surface* surface = TTF_RenderUTF8_Shaded(m_pFont, "한글 한글", color_White,color_Black ); 
-    text_texture = SDL_CreateTextureFromSurface(g_pRenderer, surface);
-    r = { 0, 0, surface->w, surface->h };
-    SDL_FreeSurface(surface);
-
 	
+    while (g_bRunning)
+    {
+        // handle input - update - render
+        handleInput();
+        update();
+        render();
+    }
+
+    SDL_Quit();
+    Mix_CloseAudio();
+    Mix_Quit();
+
+    TTF_CloseFont(m_pFont);
+    TTF_Quit();
+
+    return 0;
+}
+
+bool init()
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+			SDL_Log("%s", SDL_GetError());
+      return false;
+		}
+
+		g_pWindow = SDL_CreateWindow( "HoseoSDL ", 
+																	SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+			 													  640, 480, SDL_WINDOW_SHOWN);
+		if (g_pWindow == 0) {
+			SDL_Log("%s", SDL_GetError());
+			return false;
+		}
+
+		g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		if (g_pRenderer == 0) {
+			SDL_Log("%s", SDL_GetError());
+			return false;
+		}
+
+		// SDL TTF 
+    if (TTF_Init() < 0)
+    {
+        std::cout << "오류: 폰트를 초기화할 수 없음: " << TTF_GetError() << std::endl;
+        return false;
+    }
+
+    m_pFont = TTF_OpenFont("./assets/NanumGothic.ttf", 20);
+    if (m_pFont == 0) {	
+				SDL_Log("%s", TTF_GetError());
+        return false;
+    }
+	
+    // SDL Mixer 
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) < 0)  {
+				SDL_Log("%s", Mix_GetError());
+        return false;
+    }
+
     return true;
+}
+
+void handleInput()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+			switch (event.type )
+			{
+				case SDL_QUIT: 
+					g_bRunning = false;
+				break;
+
+				case SDL_MOUSEBUTTONDOWN: 
+					if (event.button.button == SDL_BUTTON_LEFT)
+					{
+							bLeftMousePressed = true;
+					}
+				break;
+
+				case SDL_MOUSEBUTTONUP: 
+					if (event.button.button == SDL_BUTTON_LEFT)
+					{
+							bLeftMousePressed = false;
+					}
+				break;
+				
+			}
+    }
+}
+
+void update()
+{
+    if (bLeftMousePressed)
+    {
+        // chunk에 담긴 사운드 재생, -1은 가장 가까운 채널, 0은 반복 횟수
+        Mix_PlayChannel(-1, chunk, 0);
+        printf("왼쪽클릭 눌림 - 사운드 재생\n");
+    }
 }
 
 void render()
 {
     SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-	
+
     SDL_RenderClear(g_pRenderer);
 
-    SDL_RenderCopy(g_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
-    SDL_RenderCopy(g_pRenderer, text_texture, &r, &r);
+    SDL_RenderCopy(g_pRenderer, texureImage, &rectImage, &rectImage);
+    SDL_RenderCopy(g_pRenderer, texureText, &rectText, &rectText);
 
-    filledCircleColor(g_pRenderer, 300, 300, 50, 0xFFFF0000); 
+    filledCircleColor(g_pRenderer, 300, 300, 50, 0xFFFF0000);
 
-/*
-	    // SDL TTF
-
-
-
-*/
-	
     SDL_RenderPresent(g_pRenderer);
-}
-
-int main(int argc, char* argv[])
-{
-    if (init("HoseoSDL ", 
-              SDL_WINDOWPOS_CENTERED,
-              SDL_WINDOWPOS_CENTERED, 
-              640, 480,
-              SDL_WINDOW_SHOWN))
-    {
-        g_bRunning = true;
-    }
-    else
-    {
-        return 1; // something's wrong
-    }
-//std::cout << "CPP TEST " << std::endl;
-    while (g_bRunning)
-    {
-        // handle input - update - render
-        render();
-    }
-
-    SDL_Quit();
-    return 0;
 }
